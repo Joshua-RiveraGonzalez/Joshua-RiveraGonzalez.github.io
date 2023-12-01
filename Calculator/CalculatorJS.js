97b4ef6314ba35d24eb262e500a3086d29372aa3
Joshua-RiveraGonzalez <152662021+Joshua-RiveraGonzalez@users.noreply.github.com>
2023-12-01 18:39:17 -0500

    Added files in calculator folder

---
+++ Calculator/CalculatorJS.js
@@ -0,0 +1,22 @@
+let display = document.getElementById('screen');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'Clear':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
